e60025c08c794c4f3b2dcdbba33630f7e835af18
hsc <time.proust@protonmail.ch>
2024-12-21 00:17:42 +0900

    Quartz sync: Dec 21, 2024, 12:17 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "à priori",
+    pageTitle: "A priori",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
